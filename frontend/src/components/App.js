7224b4987a975eb24a96e3916ab88f397009e951
MohamadKazemiRe9 <mohammadkazemire9@gmail.com>
2020-07-03 20:44:50 +0430

    first

---
+++ frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <div>
+            Django React
+        </div>
+    )
+}
+
+ReactDOM.render(<App />,document.getElementById("root"));
